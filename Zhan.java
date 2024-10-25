0a3ffaf9cdbd27b36513f6255c05b2a0b095094b
Guilherme Meyer <130676425+guimeyer2@users.noreply.github.com>
2024-10-25 13:40:25 -0300

    Adição do enunciado

--- Zhan.java
+++ Zhan.java
@@ -1,3 +1,18 @@
+/*Today, a club fair was held at "NSPhM". In order to advertise his pastry club, Zhan decided to demonstrate the power of his blender.
+
+To demonstrate the power of his blender, Zhan has n
+ fruits.
+
+The blender can mix up to x
+ fruits per second.
+
+In each second, Zhan can put up to y
+ fruits into the blender. After that, the blender will blend min(x,c)
+ fruits, where c
+ is the number of fruits inside the blender. After blending, blended fruits are removed from the blender.
+
+Help Zhan determine the minimum amount of time required for Zhan to blend all fruits. */
+
 import java.util.Scanner;
 
 public class Zhan {
